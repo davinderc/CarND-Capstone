75e9c30bfa573e897b17b84df5698aab5d77cea3
Davinder Chandhok <davinderc@users.noreply.github.com>
2018-05-20 01:50:43 -0300

    Add classification code and simulator model

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,13 +1,30 @@
 from styx_msgs.msg import TrafficLight
-from keras.models import load_model
-import h5py
+import tensorflow as tf
 
 class TLClassifier(object):
-    def __init__(self):
+    def __init__(self, sim_true):
         #TODO load classifier
+        if sim_true:
+            GRAPH_PATH = r'light_classification/model/inf_graph_sim.pb'
+        else:
+            GRAPH_PATH = r'light_classification/model/inf_graph_udacity.pb'
+        self.graph = tf.Graph()
         
-        model = load_model('model.h5')
-        pass
+        
+        with graph.as_default():
+            new_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(graph_file, 'rb') as fid:
+                serialized_graph = fid.read()
+                new_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(new_graph_def, name='')
+            
+        self.image_tensor = self.graph.get_tensor_by_name('image_tensor:0')
+        self.boxes = self.graph.get_tensor_by_name('detection_boxes:0')
+        self.scores = self.graph.get_tensor_by_name('detection_scores:0')
+        self.classes = self.graph.get_tensor_by_name('detection_classes:0')
+        self.num_detections = self.graph.get_tensor_by_name('num_detections:0')
+            
+        self.sess = tf.Session(graph=self.graph)
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -22,12 +39,26 @@ class TLClassifier(object):
         #TODO implement light color prediction
         #  Check what the styx_msgs/TrafficLight msg should look like to 
         #  create the correct output
-        light_color = model.predict(image)
-        if(light_color == 4):
+        
+        with self.graph.as_default():
+            img_expand = np.expand_dims(image, axis=0)
+            start = datetime.datetime.now()
+            (boxes, scores, classes, num_detections) = self.sess.run(
+                [self.boxes, self.scores, self.classes, self.num_detections], feed_dict={self.image_tensor: img_expand})
+            end = datetime.datetime.now()
+            duration = end - start
+            print(duration.total_seconds())
+        
+        boxes = np.squeeze(boxes)
+        scores = np.squeeze(scores)
+        classes = np.squeeze(classes).astype(np.int32)
+        
+        if(scores[0] > self.thresh):
+            if(classes[0] == 1):
+                return TrafficLight.GREEN
+            elif(classes[0] == 2):
+                return TrafficLight.RED
+            elif (classes[0] == 3):
+                return TrafficLight.YELLOW
+        else:
             return TrafficLight.UNKNOWN
-        if(light_color == 2):
-            return TrafficLight.GREEN
-        if(light_color == 1):
-            return TrafficLight.YELLOW
-        if(light_color == 0):
-            return TrafficLight.RED
